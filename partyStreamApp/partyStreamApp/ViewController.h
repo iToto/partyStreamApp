b4e64d35d23ddebd11024cb37a509e6324f7c98e
Salvatore D'Agositno <toto19@gmail.com>
2013-02-25 19:34:48 -0500

    converted to single view application
    Going to start off simple

---
+++ partyStreamApp/partyStreamApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  partyStreamApp
+//
+//  Created by Salvatore D'Agostino on 2013-02-25.
+//  Copyright (c) 2013 Salvatore D'Agostino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ partyStreamApp/partyStreamApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  partyStreamApp
+//
+//  Created by Salvatore D'Agostino on 2013-02-25.
+//  Copyright (c) 2013 Salvatore D'Agostino. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
