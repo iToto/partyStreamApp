4ab3a6895960814b32901e1ebe4a63e2a7ec221d
Salvatore D'Agositno <toto19@gmail.com>
2013-04-07 16:16:32 -0400

    initial import of the ListEventsViewController

---
+++ partyStreamApp/partyStreamApp/ListEventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListEventsViewController.h
+//  partyStreamApp
+//
+//  Created by Salvatore D'Agostino on 2013-04-07.
+//  Copyright (c) 2013 Salvatore D'Agostino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListEventsViewController : UIViewController
+
+@end
---
+++ partyStreamApp/partyStreamApp/ListEventsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ListEventsViewController.m
+//  partyStreamApp
+//
+//  Created by Salvatore D'Agostino on 2013-04-07.
+//  Copyright (c) 2013 Salvatore D'Agostino. All rights reserved.
+//
+
+#import "ListEventsViewController.h"
+
+@interface ListEventsViewController ()
+
+@end
+
+@implementation ListEventsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
