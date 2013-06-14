250d4b9adb35bb9043f63ea295376314d3af3643
mdewolfe <dewolfe.michael@gmail.com>
2013-06-13 20:21:08 -0400

    Added logic test target for building data models.

---
+++ partyStreamApp/PSModelTests/PSModelTests.h
@@ -0,0 +1,13 @@
+//
+//  PSModelTests.h
+//  PSModelTests
+//
+//  Created by Mike De Wolfe on 2013-06-13.
+//  Copyright (c) 2013 Salvatore D'Agostino. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PSModelTests : SenTestCase
+
+@end
---
+++ partyStreamApp/PSModelTests/PSModelTests.m
@@ -0,0 +1,32 @@
+//
+//  PSModelTests.m
+//  PSModelTests
+//
+//  Created by Mike De Wolfe on 2013-06-13.
+//  Copyright (c) 2013 Salvatore D'Agostino. All rights reserved.
+//
+
+#import "PSModelTests.h"
+
+@implementation PSModelTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PSModelTests");
+}
+
+@end
